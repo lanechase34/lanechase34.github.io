95677a16fd953367e376b4aeafd20821f8a9ecc2
lanechase34 <lanechase34@outlook.com>
2022-03-30 16:25:51 -0400

    revamped website

---
+++ app.js
@@ -0,0 +1,9 @@
+const resumeBtn = document.querySelector('#resume');
+resumeBtn.addEventListener('click', function () {
+    document.location.href = 'static/Chase_Lane_Resume.pdf';
+});
+
+const projBtn = document.querySelector('#projects');
+projBtn.addEventListener('click', function () {
+    document.location.href = 'projects.html';
+});
