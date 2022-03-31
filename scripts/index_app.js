91d008f9fde45cba9bf2c39b4f2c1ac61610843a
lanechase34 <lanechase34@hotmail.com>
2022-03-31 11:41:16 -0400

    progress

---
+++ scripts/index_app.js
@@ -0,0 +1,20 @@
+const resumeBtn = document.querySelector('#resume');
+resumeBtn.addEventListener('click', function () {
+    document.location.href = 'static/Chase_Lane_Resume.pdf';
+});
+// const projBtn = document.querySelector('#projects');
+// projBtn.addEventListener('click', function () {
+//     document.location.href = 'projects.html';
+// });
+const linkedBtn = document.querySelector('#LinkedIn');
+linkedBtn.addEventListener('click', function () {
+    document.location.href = 'https://www.linkedin.com/in/chase-lane-532217220/';
+});
+const gitBtn = document.querySelector('#Github');
+gitBtn.addEventListener('click', function () {
+    document.location.href = 'https://github.com/lanechase34';
+});
+const mailBtn = document.querySelector('#Email');
+mailBtn.addEventListener('click', function () {
+    document.location.href = 'mailto:lanechase34@outlook.com';
+});
---
+++ scripts/projects_app.js
@@ -0,0 +1,10 @@
+const homeBtn = document.querySelector('#home');
+homeBtn.addEventListener('click', function () {
+    document.location.href = 'index.html';
+});
+
+const projBtn = document.querySelector('#projects');
+projBtn.addEventListener('click', function () {
+    document.location.href = 'projects.html';
+});
+
