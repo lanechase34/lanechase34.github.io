ec376261802986233684d42f7f7414971861e763
lanechase34 <lanechase34@hotmail.com>
2022-04-07 23:06:04 -0400

    updated projects page

--- scripts/index_app.js
+++ scripts/index_app.js
@@ -2,10 +2,10 @@ const resumeBtn = document.querySelector('#resume');
 resumeBtn.addEventListener('click', function () {
     document.location.href = 'static/Chase_Lane_Resume.pdf';
 });
-// const projBtn = document.querySelector('#projects');
-// projBtn.addEventListener('click', function () {
-//     document.location.href = 'projects.html';
-// });
+const projBtn = document.querySelector('#projects');
+projBtn.addEventListener('click', function () {
+    document.location.href = 'projects.html';
+});
 const linkedBtn = document.querySelector('#LinkedIn');
 linkedBtn.addEventListener('click', function () {
     document.location.href = 'https://www.linkedin.com/in/chase-lane-532217220/';
